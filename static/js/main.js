ca169c492740a2db848ea0560717ba22081e6643
Coraline Ada Ehmke <coraline@idolhands.com>
2020-02-08 17:22:49 -0600

    Remove unused JS

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@
 // init foundation
 // $(document).foundation();
 
-// Init all plugin when document is ready 
+// Init all plugin when document is ready
 $(document).on('ready', function () {
 	// 0. Init console to avoid error
 	var method;
@@ -27,7 +27,7 @@ $(document).on('ready', function () {
 		}
 	}
 
-	// 1. Background image as data attribut 
+	// 1. Background image as data attribut
 	var list = $('.bg-img');
 	for (var i = 0; i < list.length; i++) {
 		var src = list[i].getAttribute('data-image-src');
@@ -36,7 +36,7 @@ $(document).on('ready', function () {
 		list[i].style.backgroundPosition = "center";
 		list[i].style.backgroundSize = "cover";
 	}
-	// Image block to Background image 
+	// Image block to Background image
 	var listImgBlock = $('.img-block');
 	for (var i = 0; i < listImgBlock.length; i++) {
 		var src = listImgBlock[i].getAttribute('src');
@@ -54,19 +54,6 @@ $(document).on('ready', function () {
 	for (var i = 0; i < listColor.length; i++) {
 		var src = listColor[i].getAttribute('data-bgcolor');
 		listColor[i].style.backgroundColor = src;
-	}
-
-	// 2. Init Coutdown clock
-	try {
-		// check if clock is initialised
-		$('.clock-countdown').downCount({
-			date: $('.site-config').attr('data-date'),
-			offset: +10
-		});
-	}
-	catch (error) {
-		// Clock error : clock is unavailable
-		console.log("clock disabled/unavailable");
 	}
 
 	// 3. Show/hide menu when icon is clicked
@@ -93,83 +80,6 @@ $(document).on('ready', function () {
 		return true;
 	});
 
-	// 4 Carousel Slider
-	
-	// 4.a carousel-alpha demo
-	$('.carousel-slick-alpha-demo').slick({
-		dots: true
-	});
-
-	// carousel-fullscreen : fullscreen projects slider
-	new Swiper('.carousel-swiper-fullscreen-demo .swiper-container', {
-		pagination: '.carousel-swiper-fullscreen-demo .items-pagination',
-		paginationClickable: '.carousel-fullscreen-demo .items-pagination',
-		nextButton: '.carousel-swiper-fullscreen-demo .items-button-next',
-		prevButton: '.carousel-swiper-fullscreen-demo .items-button-prev',
-		loop: true,
-		grabCursor: true,
-		centeredSlides: false,
-		autoplay: 5000,
-		autoplayDisableOnInteraction: false,
-		slidesPerView: 2,
-		spaceBetween: 16,
-		effect: 'slide',
-		breakpoints: {
-			440: {
-				slidesPerView: 1,
-				spaceBetween: 0
-			}
-		}
-	});
-	// carousel-beta :projects list slider
-	new Swiper('.carousel-swiper-beta-demo .swiper-container', {
-		pagination: '.carousel-swiper-beta-demo .items-pagination',
-		paginationClickable: '.carousel-beta-demo .items-pagination',
-		nextButton: '.carousel-swiper-beta-demo .items-button-next',
-		prevButton: '.carousel-swiper-beta-demo .items-button-prev',
-		loop: true,
-		grabCursor: true,
-		centeredSlides: false,
-		autoplay: 5000,
-		autoplayDisableOnInteraction: false,
-		slidesPerView: 2,
-		spaceBetween: 0,
-		breakpoints: {
-			1024: {
-				slidesPerView: 2,
-			},
-			800: {
-				slidesPerView: 1,
-				spaceBetween: 0
-			},
-			640: {
-				slidesPerView: 1,
-				spaceBetween: 0
-			},
-			440: {
-				slidesPerView: 1,
-				spaceBetween: 0
-			}
-		}
-	});
-	
-	// 4.1 Slideshow slider
-	var imageList = $('.slide-show .img');
-	var imageSlides = [];
-	for (var i = 0; i < imageList.length; i++) {
-		var src = imageList[i].getAttribute('data-src');
-		imageSlides.push({ src: src });
-	}
-	$('.slide-show').vegas({
-		delay: 5000,
-		shuffle: true,
-		slides: imageSlides,
-		animation: ['kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight']
-	});
-	
-	// 5. Init video background
-	var videoBg = $('.video-container video, .video-container object');
-
 	// 6. Prepare content for animation
 	$('.section .content .anim.anim-wrapped').wrap("<span class='anim-wrapper'></span>");
 
@@ -190,9 +100,6 @@ $(document).on('ready', function () {
 	var nextSection;
 	var fpnavItem;
 	var mainPage = $('#mainpage');
-	var galleryPage = $('#gallerypage');
-	var sendEmailForm = $('.send_email_form');
-	var sendMessageForm = $('.send_message_form');
 	var scrollOverflow = true;
 	var css3 = true;
 	// disable scroll overflow on small device
@@ -233,42 +140,7 @@ $(document).on('ready', function () {
 				},
 				normalScrollElements: '.section .scrollable',
 				afterRender: function () {
-					// init parallax 
-					var parallaxCover = document.getElementById('parallax-cover')
-					if (parallaxCover) {
-						if (contextWindow.width() > 1024) {
-							var parallaxInstance = new Parallax(parallaxCover);
-						}
-					}
-
-					// init sliders
-					
-					// carousel-alpha : team about us
-					new Swiper('.carousel-swiper-alpha-demo .swiper-container', {
-						pagination: '.carousel-swiper-alpha-demo .items-pagination',
-						paginationClickable: '.carousel-alpha-demo .items-pagination',
-						nextButton: '.carousel-swiper-alpha-demo .items-button-next',
-						prevButton: '.carousel-swiper-alpha-demo .items-button-prev',
-						loop: true,
-						grabCursor: true,
-						centeredSlides: false,
-						autoplay: 5000,
-						autoplayDisableOnInteraction: false,
-						slidesPerView: 2,
-						spaceBetween: 16,
-						effect: 'slide',
-						breakpoints: {
-							440: {
-								slidesPerView: 1,
-								spaceBetween: 0
-							}
-						}
-					});
-
-					
-					// Fix video background
-					videoBg.maximage('maxcover');
-
+					// init parallax
 					// Fix for internet explorer : adjust content height
 					// Detect IE 6-11
 					var isIE = /*@cc_on!@*/false || !!document.documentMode;
@@ -281,27 +153,6 @@ $(document).on('ready', function () {
 							}
 						}
 					}
-
-					// init contact form
-					// Default server url
-					var newsletterServerUrl = './ajaxserver/serverfile.php';
-					var messageServerUrl = './ajaxserver/serverfile.php';
-
-					// Use form define action attribute
-					if (sendEmailForm.attr('action') && (sendEmailForm.attr('action')) != '') {
-						newsletterServerUrl = sendEmailForm.attr('action');
-					}
-					if (sendMessageForm.attr('action') && (sendMessageForm.attr('action') != '')) {
-						messageServerUrl = sendMessageForm.attr('action');
-					}
-
-					sendEmailForm.initForm({
-						serverUrl: newsletterServerUrl,
-					});
-					sendMessageForm.initForm({
-						serverUrl: messageServerUrl,
-					});
-
 				},
 				afterResize: function () {
 					var pluginContainer = $(this);
@@ -340,51 +191,13 @@ $(document).on('ready', function () {
 					if (!activeSection.hasClass('section-anim')) {
 						// uncomment below for onetime animation
 						activeSection.addClass('section-anim');
-					} 
+					}
 					// hide or show clock
 					if ($('.section.active').hasClass('hide-clock')) {
 						headerContainer.addClass('gone');
 					} else {
 						headerContainer.removeClass('gone');
 					}
-				}
-			});
-
-		}
-
-		// Init galleryPage
-		if (galleryPage.width()) {
-			// config fullpage.js
-			galleryPage.fullpage({
-				menu: '#qmenu',
-				anchors: pageAnchors,
-				verticalCentered: false,
-				css3: css3,
-				navigation: true,
-				scrollOverflow: false,
-				loopTop: true,
-				loopBottom: true,
-				responsiveHeight: 480,
-				normalScrollElements: '.section .scrollable',
-				scrollOverflowOptions: {
-					click: false,
-					submit: true,
-				},
-				afterRender: function () {
-				},
-				afterResize: function () {
-					var pluginContainer = $(this);
-					$.fn.fullpage.reBuild();
-				},
-				afterLoad: function () {
-					
-				},
-				onLeave: function (index, nextIndex, direction) {
-					// Behavior when a full page is leaved
-					arrowElem.addClass('gone');
-					pageElem.addClass('transition');
-					slideElem.removeClass('transition');
-					pageElem.removeClass('transition');
 				}
 			});
 
@@ -434,21 +247,4 @@ $(document).on('ready', function () {
 		}
 	});
 
-
-	// 9. Page Loader : hide loader when all are loaded
-	contextWindow.on('load', function () {
-		$('#page-loader').addClass('p-hidden');
-		$('.section').addClass('anim');
-	});
-
-	// 10. cursor position
-	var shadowBall = $(".cursor-ball");
-	$(".body-page").mousemove(function(e) {
-		shadowBall.css("transform", "translateX(" + e.pageX + "px)");
-		// shadowBall.css("transform", "translate(" + e.pageX + "px," + e.pageY +"px)");
-		// shadowBall.posx.value = e.pageX;
-		// shadowBall.posy.value = e.pageY;
-	});
-
 });
-
